82b17216c8fa7d8009b70b0dac1be88dbaae8b31
Devildrake <divadarrei@gmail.com>
2020-11-02 21:05:56 +0100

    Established preprocessor #if _DEBUG and GL debug callback

--- Engine/ModuleHelloTriangleExcerise.cpp
+++ Engine/ModuleHelloTriangleExcerise.cpp
@@ -10,8 +10,19 @@ ModuleHelloTriangleExcercise::~ModuleHelloTriangleExcercise() {
 
 }
 
-bool ModuleHelloTriangleExcercise::Init() {
+#if _DEBUG
+void __stdcall OurOpenGLErrorFunction(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, const void* userParam);
+#endif
 
+bool ModuleHelloTriangleExcercise::Init() {
+#if _DEBUG
+	LOG("DEBUG enabled");
+	glEnable(GL_DEBUG_OUTPUT);
+	glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+	glDebugMessageCallback(&OurOpenGLErrorFunction, nullptr);
+	glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, nullptr,
+		true);
+#endif
 	//SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
 	//SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
 
@@ -112,9 +123,9 @@ unsigned ModuleHelloTriangleExcercise::CompileShader(unsigned type, const char* 
 			int written = 0;
 			char* info = (char*)malloc(len);
 			glGetShaderInfoLog(shader_id, len, &written, info);
-			//#if DEBUG
+#if _DEBUG
 			LOG("Log Info: %s", info);
-			//#endif
+#endif
 
 			free(info);
 		}
@@ -141,7 +152,7 @@ unsigned ModuleHelloTriangleExcercise::CreateProgram(unsigned vtx_shader, unsign
 			char* info = (char*)malloc(len);
 
 			glGetProgramInfoLog(program_id, len, &written, info);
-#if DEBUG
+#if _DEBUG
 			LOG("Program Log Info: %s", info);
 #endif
 
@@ -165,3 +176,37 @@ void ModuleHelloTriangleExcercise::RenderVBO(unsigned vbo, unsigned program)
 	// 1 triangle to draw = 3 vertices
 	glDrawArrays(GL_TRIANGLES, 0, 3);
 }
+
+#if _DEBUG
+
+void __stdcall OurOpenGLErrorFunction(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, const void* userParam)
+{
+	const char* tmp_source = "", * tmp_type = "", * tmp_severity = "";
+	switch (source) {
+	case GL_DEBUG_SOURCE_API: tmp_source = "API"; break;
+	case GL_DEBUG_SOURCE_WINDOW_SYSTEM: tmp_source = "Window System"; break;
+	case GL_DEBUG_SOURCE_SHADER_COMPILER: tmp_source = "Shader Compiler"; break;
+	case GL_DEBUG_SOURCE_THIRD_PARTY: tmp_source = "Third Party"; break;
+	case GL_DEBUG_SOURCE_APPLICATION: tmp_source = "Application"; break;
+	case GL_DEBUG_SOURCE_OTHER: tmp_source = "Other"; break;
+	};
+	switch (type) {
+	case GL_DEBUG_TYPE_ERROR: tmp_type = "Error"; break;
+	case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR: tmp_type = "Deprecated Behaviour"; break;
+	case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR: tmp_type = "Undefined Behaviour"; break;
+	case GL_DEBUG_TYPE_PORTABILITY: tmp_type = "Portability"; break;
+	case GL_DEBUG_TYPE_PERFORMANCE: tmp_type = "Performance"; break;
+	case GL_DEBUG_TYPE_MARKER: tmp_type = "Marker"; break;
+	case GL_DEBUG_TYPE_PUSH_GROUP: tmp_type = "Push Group"; break;
+	case GL_DEBUG_TYPE_POP_GROUP: tmp_type = "Pop Group"; break;
+	case GL_DEBUG_TYPE_OTHER: tmp_type = "Other"; break;
+	};
+	switch (severity) {
+	case GL_DEBUG_SEVERITY_HIGH: tmp_severity = "high"; break;
+	case GL_DEBUG_SEVERITY_MEDIUM: tmp_severity = "medium"; break;
+	case GL_DEBUG_SEVERITY_LOW: tmp_severity = "low"; break;
+	case GL_DEBUG_SEVERITY_NOTIFICATION: tmp_severity = "notification"; break;
+	};
+	LOG("<Source:%s> <Type:%s> <Severity:%s> <ID:%d> <Message:%s>\n", tmp_source, tmp_type, tmp_severity, id, message);
+}
+#endif
